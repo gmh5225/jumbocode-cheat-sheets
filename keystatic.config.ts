1cf81fbf76f4dcb08ca9732a6bb995faa929f9f8
Ben Borgers <borgersbenjamin@gmail.com>
2023-07-14 12:32:02 -0400

    Install Keystatic

---
+++ keystatic.config.ts
@@ -0,0 +1,27 @@
+import { config, fields, collection } from "@keystatic/core";
+
+export default config({
+  storage: {
+    kind: "local",
+  },
+  collections: {
+    docs: collection({
+      label: "Docs",
+      slugField: "title",
+      schema: {
+        title: fields.slug({ name: { label: "Title" } }),
+        body: fields.document({
+          label: "Body",
+          formatting: true,
+          links: true,
+          dividers: true,
+          tables: true,
+          images: {
+            directory: "public/docs",
+            publicPath: "/docs",
+          },
+        }),
+      },
+    }),
+  },
+});
