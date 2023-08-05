68716a061b733ef77c85efe1d5060c779ca49a57
Ben Borgers <borgersbenjamin@gmail.com>
2023-08-04 23:29:15 -0400

    Generate docs pages to start

--- app/(site)/[slug]/page.tsx
+++ app/(site)/[slug]/page.tsx
@@ -3,6 +3,9 @@ import { createReader } from "@keystatic/core/reader";
 import config from "../../../keystatic.config";
 import { DocumentRenderer } from "@keystatic/core/renderer";
 
+// We know all params to start, so all others can 404.
+export const dynamicParams = false;
+
 const reader = createReader(process.cwd(), config);
 
 export default async function DocPage({
@@ -23,4 +26,10 @@ export default async function DocPage({
       <DocumentRenderer document={await doc.body()} />
     </div>
   );
+}
+
+export async function generateStaticParams() {
+  const docs = await reader.collections.docs.list();
+
+  return docs.map((slug) => ({ slug }));
 }
